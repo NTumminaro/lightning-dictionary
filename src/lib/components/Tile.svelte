cc5695f977ae6f640a0e92c0d2cbf2699416120c
Nick <nconkl1@outlook.com>
2023-11-03 22:49:05 -0400

    Created Tile component
    - Created the Tile component
    - TODO: Add click functionality

---
+++ src/lib/components/Tile.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  export let entityName: string;
+</script>
+
+<div class="shadow-lg rounded-lg h-full hover:transition-colors duration-700 bg-neutral text-neutral-content hover:bg-secondary-focus p-4 text-center justify-center items-center flex cursor-pointer">
+  <h1 class="text-lg font-semibold select-none">{entityName}</h1>
+</div>
